3e7b50b104db4fd4d5eb2fee7bcf410ed51acf4b
Horácio Dias <horacio.dias@yahoo.com>
2017-08-20 01:18:24 -0300

    optimize imports

--- src/main/java/br/org/springmvc/configuration/AppConfiguration.java
+++ src/main/java/br/org/springmvc/configuration/AppConfiguration.java
@@ -20,8 +20,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
